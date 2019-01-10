feef74f76000e23faed178a8f64867c68cfec53c
Oleksandr Kovalchuk <anxolerd@outlook.com>
2019-01-11 01:41:27 +0200

    Add log when app started

--- run.py
+++ run.py
@@ -5,11 +5,14 @@ from aiohttp.web import run_app
 
 from sqli.app import init as init_app
 
+log = logging.getLogger(__name__)
+
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
 
     app = init_app(sys.argv[1:])
 
-    run_app(app,
-            host=app['config']['app']['host'],
-            port=app['config']['app']['port'])
+    host = app['config']['app']['host']
+    port = app['config']['app']['port']
+    log.info(f'App is listening at http://{host}:{port}')
+    run_app(app, host=host, port=port)
